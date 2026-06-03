54cdb32d393185fb14651cebcc7d9da62ed16939
verloren812 <nazarsysenko@gmail.com>
2026-06-03 20:15:06 +0200

    Update app.py

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_url_path='', static_folder='.')
 
 @app.route('/')
 def index():
