d94a06b57d36fb033d6dd57702c1b4882b13cdf8
verloren812 <nazarsysenko@gmail.com>
2026-06-03 18:40:34 +0200

    Update app.py

--- portfolio/app.py
+++ portfolio/app.py
@@ -30,31 +30,31 @@ def index():
             {
                 "title": " Product review/showcase",
                 "platform": "TikTok",
-                "url": "https://www.tiktok.com/@nawyfps/video/7632614572423253281" # Твоя ссылка
+                "url": "https://www.tiktok.com/@nawyfps/video/7632614572423253281", # Твоя ссылка
                 "image": "https://via.placeholder.com/400x225?text=TikTok+Video"
             },
             {
                 "title": "Silo Demo Showcase",
                 "platform": "YouTube",
-                "url": " https://youtu.be/rIqoK-alAXM" # Ссылка на любой ТикТок
+                "url": " https://youtu.be/rIqoK-alAXM", # Ссылка на любой ТикТок
                 "image": "https://img.youtube.com/vi/riQoK-aLAXM/mqdefault.jpg"
             },
             {
                 "title": "Viral YouTube Project",
                 "platform": "YouTube",
-                "url": " https://youtu.be/8x1Nbj4J3pc?si=nArL0yV75mirbfc7" # Твоя ссылка
+                "url": " https://youtu.be/8x1Nbj4J3pc?si=nArL0yV75mirbfc7", # Твоя ссылка
                 "image": "https://img.youtube.com/vi/8x1Nbj4J3pc/mqdefault.jpg"
             },
           {
                 "title": " Performance Marketing TikTok Ad",
                 "platform": "TikTok",
-                "url": "https://www.tiktok.com/@aimstarshq/video/7621939553061965089"
+                "url": "https://www.tiktok.com/@aimstarshq/video/7621939553061965089",
                 "image": "https://via.placeholder.com/400x225?text=TikTok+Video"
             },
             {
                 "title": "Top-Converting Social Commerce Edit",
                 "platform": "YouTube",
-                "url": "https://youtu.be/SvGQ9py24gY?si=VNXZgsyrlgybnhoB"
+                "url": "https://youtu.be/SvGQ9py24gY?si=VNXZgsyrlgybnhoB",
                 "image": "https://img.youtube.com/vi/SvGQ9py24gY/mqdefault.jpg"
             }
         ]
